c65cefb746e04e4666f0c0800e0eee41d495f65d
Ayamarmash <58006991+Ayamarmash@users.noreply.github.com>
2022-02-24 22:12:11 +0200

    Add project files.

---
+++ MyApplication/MyApplication/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MyApplication
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            String MyDate = DateTime.Now.ToString();
+            MyLabel.Text = "Aya Marmash" + "\n" + MyDate;
+        }
+
+        private void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            WebPage.Source = "https://zajel.najah.edu/";
+        }
+
+        private void ImageButton_Clicked_1(object sender, EventArgs e)
+        {
+            WebPage.Source = "https://google.ps";
+        }
+    }
+}
